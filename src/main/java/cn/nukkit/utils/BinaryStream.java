dc798c57927df182933a3cc52932849d02145ebe
3zero <44516212+3zeroed@users.noreply.github.com>
2022-05-14 15:59:34 +0800

    Fixed NullPointerException: "java.lang.Integer.intValue()" because "id" is null 	at cn.nukkit.utils.BinaryStream.getSlot(BinaryStream.java:453)

--- src/main/java/cn/nukkit/utils/BinaryStream.java
+++ src/main/java/cn/nukkit/utils/BinaryStream.java
@@ -7,7 +7,10 @@ import cn.nukkit.blockstate.BlockState;
 import cn.nukkit.blockstate.BlockStateRegistry;
 import cn.nukkit.entity.Attribute;
 import cn.nukkit.entity.data.Skin;
-import cn.nukkit.item.*;
+import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemDurable;
+import cn.nukkit.item.ItemID;
+import cn.nukkit.item.RuntimeItems;
 import cn.nukkit.level.GameRule;
 import cn.nukkit.level.GameRules;
 import cn.nukkit.math.BlockFace;
@@ -450,9 +453,7 @@ public class BinaryStream {
 
             if (compoundTag != null && compoundTag.getAllTags().size() > 0) {
                 if (compoundTag.contains("Damage")) {
-                    if (id > 255) {
-                        damage = compoundTag.getInt("Damage");
-                    }
+                    damage = compoundTag.getInt("Damage");
                     compoundTag.remove("Damage");
                 }
                 if (compoundTag.contains("__DamageConflict__")) {
@@ -483,9 +484,9 @@ public class BinaryStream {
         }
 
         Item item = null;
-        if(id != null) {
+        if (id != null) {
             item = readUnknownItem(Item.get(id, damage, count, nbt));
-        }else if(stringId != null) {
+        } else if (stringId != null) {
             final Item tmp = Item.fromString(stringId);
             tmp.setDamage(damage);
             tmp.setCount(count);
